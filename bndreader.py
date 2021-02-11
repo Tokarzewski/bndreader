543b98293feb8d7a23e9a1cc267f555a946dd78b
tokarzewski <bartlomiej.tokarzewski@gmail.com>
2021-02-11 20:42:21 +0100

    preprocess changes
    function changed to include comments and scheme

--- bndreader.py
+++ bndreader.py
@@ -1,7 +1,19 @@
 
 def preprocess(text):
     text = text.strip().split("\n")
-    return [x.strip().split(",") for x in text if "#" not in x[0:2] and "!" not in x[0:2]]
+    content, comments, scheme = [], [], []
+    for x in text:
+        x=x.strip()
+        if "#" not in x[0] and "!" not in x[0]:
+            content.append(x.split(","))
+        else:
+            comments.append(x.split(","))
+            if "! <" in x[0:3] and "! <#" not in x[0:4]:
+                char_to_replace = {'! ': '','#': '', '<':'', '>':''}
+                for key, value in char_to_replace.items():
+                    x = x.replace(key, value)
+                scheme.append(x.split(","))
+    return content, comments, scheme
 
 class parser:
     def __init__(self, filepath):
@@ -10,18 +22,18 @@ class parser:
         separator = "! ==============================================================="
         text_list = text.split(separator)
 
-        self.ProgramVersion             = preprocess(text_list[0])
-        self.Nodes                      = preprocess(text_list[1])
-        self.SuspiciousNodes            = preprocess(text_list[2])
-        self.BranchLists                = preprocess(text_list[3])
-        self.SupplyAirPaths             = preprocess(text_list[4])
-        self.ReturnAirPaths             = preprocess(text_list[5])
-        self.OutdoorAirNodes            = preprocess(text_list[6])
-        self.ComponentSets              = preprocess(text_list[7])
-        self.PlantLoops                 = preprocess(text_list[8])
-        self.CondenserLoops             = preprocess(text_list[9])
-        self.ControlledZones            = preprocess(text_list[10])
-        self.ZoneEquipmentLists         = preprocess(text_list[11])
-        self.AirLoopHVACs               = preprocess(text_list[12])
-        self.ParentNodeConnections      = preprocess(text_list[13])
-        self.NonParentNodeConnections   = preprocess(text_list[14])
+        self.ProgramVersion             = preprocess(text_list[0])  #scheme count
+        self.Nodes                      = preprocess(text_list[1])  #1
+        self.SuspiciousNodes            = preprocess(text_list[2])  #1
+        self.BranchLists                = preprocess(text_list[3])  #2
+        self.SupplyAirPaths             = preprocess(text_list[4])  #3-4
+        self.ReturnAirPaths             = preprocess(text_list[5])  #3-4
+        self.OutdoorAirNodes            = preprocess(text_list[6])  #0-1
+        self.ComponentSets              = preprocess(text_list[7])  #1
+        self.PlantLoops                 = preprocess(text_list[8])  #6
+        self.CondenserLoops             = preprocess(text_list[9])  #6
+        self.ControlledZones            = preprocess(text_list[10]) #3
+        self.ZoneEquipmentLists         = preprocess(text_list[11]) #2
+        self.AirLoopHVACs               = preprocess(text_list[12]) #9
+        self.ParentNodeConnections      = preprocess(text_list[13]) #1
+        self.NonParentNodeConnections   = preprocess(text_list[14]) #1
--- test_project.py
+++ test_project.py
@@ -1,5 +1,40 @@
 import bndreader
 
-filepath = "Boiler_Convector.bnd"
-bnd = bndreader.parser(filepath)
-print(bnd.PlantLoops[0])
+filepath1 = "Boiler_Convector.bnd"
+filepath2 = "DOAS_Preheat.bnd"
+filepath3 = "FCU_Boiler_Chiller.bnd"
+filepath4 = "PTHP.bnd"
+
+bnd = bndreader.parser(filepath2)
+
+#1 content
+#print(bnd.Nodes[0])
+
+#2 comments
+#print(bnd.Nodes[1])
+
+#3 scheme
+#print(bnd.Nodes[2])
+
+#4
+#print(*bnd.AirLoopHVACs[2], sep = "\n")
+
+#5
+#print(*bnd.PlantLoops, sep="\n")
+
+#6 test scheme
+"""
+outputs = {'Nodes':1, 'SuspiciousNodes':1, 'BranchLists':2,
+            'SupplyAirPaths':4, 'ReturnAirPaths':4, 'OutdoorAirNodes':1,
+            'ComponentSets':1, 'PlantLoops':6, 'CondenserLoops':6, 
+            'ControlledZones':3, 'ZoneEquipmentLists':2, 'AirLoopHVACs':9,
+            'ParentNodeConnections':1, 'NonParentNodeConnections':1}
+
+for output, value in outputs.items():
+    print(output,": ",value, len(eval('bnd.'+str(output)+'[2]')))
+"""
+#7 show scheme and content 
+print("--Scheme--")
+print(*bnd.AirLoopHVACs[2], sep = "\n")
+print("--Content--")
+print(*bnd.AirLoopHVACs[0], sep = "\n")
