a4322ef0dc68353c695bf4323313b00bea994c1a
tokarzewski <bartlomiej.tokarzewski@gmail.com>
2022-03-26 00:43:23 +0100

    Refactoring
    Functions were moved from the test project to the bndreader. The eval function was replaced with getattr. Variables and functions now have more meaningful names. Save bnd to json format is now built in function.

--- bndreader.py
+++ bndreader.py
@@ -1,44 +1,82 @@
-
 def preprocess(fragment):
     fragment = fragment.strip().split("\n")
-    content, comments, scheme = [], [], []
+    content, scheme = [], []
     for line in fragment:
         line=line.strip()
         if "#" not in line[0] and "!" not in line[0]:
             content.append(line.split(","))
         else:
-            comments.append(line.split(","))
             if "! <" in line[0:3] and "! <#" not in line[0:4]:
                 char_to_replace = {'! ': '','# ': '',' #' : '', '<':'', '>':''}
                 for key, value in char_to_replace.items():
                     line = line.replace(key, value)
                 scheme.append(line.split(","))
-    return content, comments, scheme
+    return scheme, content 
 
 class parser:
     def __init__(self, filepath):
+        self.filepath = filepath
         file_content = open(filepath, "r").read()
         separator = "! ==============================================================="
-        fragments = file_content.split(separator)
+        Outputs = file_content.split(separator)
 
-        self.ProgramVersion             = preprocess(fragments[0])  #scheme count
-        self.Nodes                      = preprocess(fragments[1])  #1
-        self.SuspiciousNodes            = preprocess(fragments[2])  #1
-        self.BranchLists                = preprocess(fragments[3])  #2
-        self.SupplyAirPaths             = preprocess(fragments[4])  #3-4
-        self.ReturnAirPaths             = preprocess(fragments[5])  #3-4
-        self.OutdoorAirNodes            = preprocess(fragments[6])  #0-1
-        self.ComponentSets              = preprocess(fragments[7])  #1
-        self.PlantLoops                 = preprocess(fragments[8])  #6
-        self.CondenserLoops             = preprocess(fragments[9])  #6
-        self.ControlledZones            = preprocess(fragments[10]) #3
-        self.ZoneEquipmentLists         = preprocess(fragments[11]) #2
-        self.AirLoopHVACs               = preprocess(fragments[12]) #9
-        self.ParentNodeConnections      = preprocess(fragments[13]) #1
-        self.NonParentNodeConnections   = preprocess(fragments[14]) #1
+        self.ProgramVersion             = preprocess(Outputs[0])  #scheme count
+        self.Nodes                      = preprocess(Outputs[1])  #1
+        self.SuspiciousNodes            = preprocess(Outputs[2])  #1
+        self.BranchLists                = preprocess(Outputs[3])  #2
+        self.SupplyAirPaths             = preprocess(Outputs[4])  #3-4
+        self.ReturnAirPaths             = preprocess(Outputs[5])  #3-4
+        self.OutdoorAirNodes            = preprocess(Outputs[6])  #0-1
+        self.ComponentSets              = preprocess(Outputs[7])  #1
+        self.PlantLoops                 = preprocess(Outputs[8])  #6
+        self.CondenserLoops             = preprocess(Outputs[9])  #6
+        self.ControlledZones            = preprocess(Outputs[10]) #3
+        self.ZoneEquipmentLists         = preprocess(Outputs[11]) #2
+        self.AirLoopHVACs               = preprocess(Outputs[12]) #9
+        self.ParentNodeConnections      = preprocess(Outputs[13]) #1
+        self.NonParentNodeConnections   = preprocess(Outputs[14]) #1
         
         self.Outputs = ['ProgramVersion','Nodes', 'SuspiciousNodes', 'BranchLists',
             'SupplyAirPaths', 'ReturnAirPaths', 'OutdoorAirNodes',
             'ComponentSets', 'PlantLoops', 'CondenserLoops', 
             'ControlledZones', 'ZoneEquipmentLists', 'AirLoopHVACs',
             'ParentNodeConnections', 'NonParentNodeConnections']
+
+def Output_schemesf(bnd, Output):
+    return getattr(bnd, Output)[0]
+
+def Output_contentsf(bnd, Output):
+    return getattr(bnd, Output)[1]
+
+def PropertyValues(bnd, Output, SchemeNr, Property):
+        Output_contents = Output_contentsf(bnd, Output)
+        Output_scheme = Output_schemesf(bnd, Output)[SchemeNr]
+        index = Output_scheme.index(Property)
+        ListOfValues = [Output_content[index] for Output_content in Output_contents if Output_content[0] == Output_scheme[0]]
+        return ListOfValues
+
+def Output_to_json(bnd, Output):
+    Output_scheme_names = [x[0] for x in Output_schemesf(bnd, Output)]
+    SchemeNr=0
+    Output_json = {}
+    for Output_scheme_name in Output_scheme_names:
+        Output_scheme = Output_schemesf(bnd, Output)[SchemeNr]
+        PropertiesValues = {Property:PropertyValues(bnd, Output, SchemeNr, Property) for Property in Output_scheme[1:]}
+        Output_json.update({Output_scheme_name:PropertiesValues})
+        SchemeNr += 1
+        #print(Output, "-", SchemeNr) #test how many schemes are in each output
+    return Output_json
+
+def bnd_json(bnd):
+    bnd_json = {Output:Output_to_json(bnd, Output) for Output in bnd.Outputs}
+    bnd_json['ProgramVersion'] = bnd.ProgramVersion[1][0][2].strip()
+    return bnd_json
+
+def save_as_json(filepath):
+    bnd = parser(filepath)
+    from os.path import splitext 
+    filename = splitext(filepath)[0]
+    with open(filename+'.json', 'w+') as file:
+        from json import dump
+        dump(bnd_json(bnd), file, indent=4)
+        print(filename+'.json saved')
---
+++ save_json.py
@@ -0,0 +1,8 @@
+import bndreader
+
+filepath1 = "Boiler_Convector.bnd"
+filepath2 = "DOAS_Preheat.bnd"
+filepath3 = "FCU_Boiler_Chiller.bnd"
+filepath4 = "PTHP.bnd"
+
+bndreader.save_as_json(filepath1)
